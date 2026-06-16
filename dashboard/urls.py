34bfa477fe2c91a12adb94bd41519057b9d09595
mentorvia <mentorvia.in@gmail.com>
2026-06-16 21:28:47 +0530

    Add files via upload

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,6 +5,7 @@ from . import views
 from interests import staff_views as interest_views
 from profiles import staff_views as profile_views
 from profiles import profile_editor
+from bookings import staff_views as booking_views
 
 app_name = "staff"
 
@@ -13,6 +14,11 @@ urlpatterns = [
     path("users/", views.users_list, name="users"),
     path("users/<int:user_id>/", views.user_detail, name="user_detail"),
     path("emails/", views.email_log, name="emails"),
+    # Bookings & money
+    path("bookings/", booking_views.bookings_list, name="bookings"),
+    path("bookings/<int:booking_id>/", booking_views.booking_detail, name="booking_detail"),
+    path("ledger/", booking_views.ledger, name="ledger"),
+    path("earnings/", booking_views.mentor_earnings_report, name="earnings_report"),
     # Interests
     path("interests/", interest_views.interests_list, name="interests"),
     path("interests/add/", interest_views.interest_add, name="interest_add"),
