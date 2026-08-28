fb698e0b7ea7128cac01e5620a2e138960ce2d64
mentorvia <mentorvia.in@gmail.com>
2026-08-28 20:36:30 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -98,6 +98,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "core.middleware.NoCacheForAuthenticatedMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
